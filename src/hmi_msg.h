c5f8a0bc8c01c6fc9e06b14d2eb9e7c5dd0c13a4
Roomet Saadi <roomet.saadi@itcollege.ee>
2016-10-06 15:01:13 +0300

    added lab 3.1

---
+++ src/hmi_msg.h
@@ -0,0 +1,14 @@
+#ifndef _HMI_MSG_H_
+#define _HMI_MSG_H_
+
+
+#define STUD_NAME "Roomet Saadi"
+
+const char *nameMonth[]= {"January",
+                            "February",
+                            "March",
+                            "April",
+                            "May",
+                            "June"};
+
+#endif 
--- src/uart.h
+++ src/uart.h
@@ -1,3 +1,6 @@
+#ifndef _UART_H_
+#define _UART_H_
+
 int uart0_putchar(char c, FILE *stream);
 int uart0_getchar(FILE *stream);
 
@@ -10,3 +13,5 @@ void uart3_init(void);
 
 FILE uart0_io = FDEV_SETUP_STREAM(uart0_putchar, uart0_getchar, _FDEV_SETUP_RW;
 FILE uart3_out = FDEV_SETUP_STREAM(uart3_putchar, NULL, _FDEV_SETUP_WRITE;
+
+#endif
