ec02e18e0cc04221e5d63283b92e0ad00efbc6be
Roomet Saadi <rsaadi@klass3-17-l.intra.itcollege.ee>
2016-11-04 15:38:29 +0200

    commit

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -4,7 +4,7 @@
 
 #define PROG_VERSION "Version: %S built on: %S %S\n"
 #define LIBC_VERSION "avr-libc version: %S\n"
-#define STUD_NAME "Roomett Saadi"
+#define STUD_NAME "Roomet Saadi"
 #define GET_MONTH_MSG "Enter Month name first letter >"
 
 const char m1[]  PROGMEM = "January";
